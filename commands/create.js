66f4d3db12b4ebb3b477b8fd5acaad26ed790617
xl-wolf <1072236984@qq.com>
2021-02-05 11:15:48 +0800

    注释剥离不必要代码和模块 加入注释

--- commands/create.js
+++ commands/create.js
@@ -1,14 +1,14 @@
 const chalk = require('chalk');
 const download = require('../utils/git-template')
 const ora = require('ora');
-const deleteFolder = require('../utils/deleteFolder');
+// const deleteFolder = require('../utils/deleteFolder');
 const tpls = require('../utils/templates.json')
 const inquirerFunc = require('../utils/inquirer')
 
 const downloadGit = (PName, template) => {
     console.log(chalk.greenBright('\n Start generating... \n'));
     // 加载图标
-    deleteFolder('./' + PName);
+    // deleteFolder('./' + PName);
     const spinner = ora('Downloading...');
     spinner.start();
     // 从vue和react两个模板中选择一个
